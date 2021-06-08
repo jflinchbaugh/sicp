afdb7115376d39529e25eb20a882cba13098af17
John Flinchbaugh <john@hjsoft.com>
2021-06-08 05:22:55 -0400

    whitespace

--- test/sicp/core_test.clj
+++ test/sicp/core_test.clj
@@ -6,4 +6,6 @@
   (testing "permutations"
     (is (= [nil] (permutations [])))
     (is (= [[1]] (permutations [1])))
-    (is (= [[1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]] (permutations [1 2 3])))))
+    (is (=
+          [[1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]]
+          (permutations [1 2 3])))))
