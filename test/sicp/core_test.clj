f867536d69fc457859d1838c03b3faf9b474f47a
John Flinchbaugh <john@hjsoft.com>
2021-06-08 05:21:14 -0400

    tests for permutations

--- test/sicp/core_test.clj
+++ test/sicp/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [sicp.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-permutations
+  (testing "permutations"
+    (is (= [nil] (permutations [])))
+    (is (= [[1]] (permutations [1])))
+    (is (= [[1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]] (permutations [1 2 3])))))
