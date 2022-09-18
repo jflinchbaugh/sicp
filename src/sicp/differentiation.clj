b4b2402c0ad6811497fda075c4d7db0f65ba0972
John Flinchbaugh <john@hjsoft.com>
2022-09-18 12:46:32 -0400

    simplify 0s and 1s

--- src/sicp/differentiation.clj
+++ src/sicp/differentiation.clj
@@ -9,7 +9,10 @@
   (and (variable? v1) (variable? v2) (= v1 v2)))
 
 (defn make-sum [a1 a2]
-  (list '+ a1 a2))
+  (cond
+    (and (number? a1) (zero? a1)) a2
+    (and (number? a2)(zero? a2)) a1
+    :else (list '+ a1 a2)))
 
 (defn sum? [x]
   (and (list? x) (= '+ (first x))))
@@ -21,7 +24,12 @@
   (nth s 2))
 
 (defn make-product [m1 m2]
-  (list '* m1 m2))
+  (cond
+    (and (number? m1) (zero? m1)) 0
+    (and (number? m2) (zero? m2)) 0
+    (and (number? m1) (= 1 m1)) m2
+    (and (number? m2) (= 1 m2)) m1
+    :else (list '* m1 m2)))
 
 (defn product? [x]
   (and (list? x) (= '* (first x))))
@@ -45,12 +53,15 @@
 (comment
 
   (deriv '(+ x 3) 'x)
+  ;; => 1
   ;; => (+ 1 0)
 
   (deriv '(* x y) 'x)
+  ;; => y
   ;; => (+ (* x 0) (* 1 y))
 
   (deriv '(* (* x y) (+ x 3)) 'x)
+  ;; => (+ (* x y) (* y (+ x 3)))
   ;; => (+ (* (* x y) (+ 1 0)) (* (+ (* x 0) (* 1 y)) (+ x 3)))
 
   .)
