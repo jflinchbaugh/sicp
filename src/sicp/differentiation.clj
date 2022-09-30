9007998369ebaa97f024b3aa7a0e45d880109158
John Flinchbaugh <john@hjsoft.com>
2022-09-30 00:51:27 -0400

    solve for exponentation

--- src/sicp/differentiation.clj
+++ src/sicp/differentiation.clj
@@ -42,14 +42,37 @@
 (defn multiplicand [p]
   (nth p 2))
 
+(defn make-exponentation [b e]
+  (cond
+    (and (number? b) (number? e)) (Math/pow b e)
+    (and (number? e) (zero? e)) 1
+    (and (number? e) (= 1 e)) b
+    :else (list '** b e)))
+
+(defn exponentation? [x]
+  (and (list? x) (= '** (first x))))
+
+(defn base [e]
+  (second e))
+
+(defn exponent [e]
+  (nth e 2))
+
 (defn deriv [exp var]
   (cond
     (number? exp) 0
     (variable? exp) (if (same-variable? exp var) 1 0)
     (sum? exp) (make-sum (deriv (addend exp) var) (deriv (augend exp) var))
     (product? exp) (make-sum
-                     (make-product (multiplier exp) (deriv (multiplicand exp) var))
-                     (make-product (deriv (multiplier exp) var) (multiplicand exp)))
+                    (make-product (multiplier exp) (deriv (multiplicand exp) var))
+                    (make-product (deriv (multiplier exp) var) (multiplicand exp)))
+    (exponentation? exp) (make-product
+                          (exponent exp)
+                          (make-product
+                            (make-exponentation
+                              (base exp)
+                              (make-sum (exponent exp) -1))
+                            (deriv (base exp) var)))
     :else (error "unknown expression type -- DERIV" exp)))
 
 (comment
@@ -68,5 +91,22 @@
 
   (deriv '(+ (* x 2) (* x 7)) 'x)
   ;; => 9
+
+  (deriv '(** x 2) 'x)
+  ;; => (* 2 x)
+
+  (deriv '(** x 3) 'x)
+  ;; => (* 3 (** x 2))
+
+  (deriv '(** (* 2 x) 3) 'x)
+  ;; => (* 3 (* (** (* 2 x) 2) 2))
+
+  (deriv '(** (+ 1 (* 3 x)) 2) 'x)
+  ;; => (* 2 (* (+ 1 (* 3 x)) 3))
+
+  (deriv '(** 3 x) 'x)
+
+  (deriv '(+ (* 3 (** x 3)) (* 5 (** x 2)) x) 'x)
+  ;; => (+ (* 3 (* 3 (** x 2))) (* 5 (* 2 x)))
 
   .)
